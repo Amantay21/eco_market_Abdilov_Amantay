49341828e4720d58b772b5f551eb2e028aaa2251
Amantay21 <amanatayabdilov21@gamil.com>
2023-12-17 15:22:11 +0600

    Создал необходимые категории и создал фикстуры

--- webapp/models.py
+++ webapp/models.py
@@ -5,9 +5,15 @@ class ProdCategory(models.Model):
     description = models.TextField(max_length=20000, null=False, blank=False, verbose_name='Описание')
     image = models.CharField(max_length=200, null=False, blank=False, verbose_name='Фото')
 
+    def __str__(self):
+        return f'{self.id}. {self.title}'
+
 
 class Product(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Название')
     image = models.CharField(max_length=200, null=False, blank=False, verbose_name='Фото')
     amount = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, verbose_name="Цена")
     prod_category = models.ForeignKey('webapp.ProdCategory', on_delete=models.RESTRICT, verbose_name='Категория')
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
